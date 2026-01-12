eb1914436775bb4fc784ddca619ea17dd9b95bf0
Sovangi <sovangipoddar@gmail.com>
2026-01-12 18:35:43 +0530

    feat: add dry run mode and refactor to main entry point

--- organizer.py
+++ organizer.py
@@ -34,47 +34,61 @@ def if_path_exists(target_path, purpose="directory"):
         return False
     
 
-config= load_config()
- 
-logging.info("***********New Scanning Started***********")
+def main():
+    config= load_config()
+    logging.info("***********New Scanning Started***********")
 
-folders=config.get("folders",{})
-path=config.get("target_path","")
+    is_dry_run=config.get("settings",{}).get("dry_run",True)
+    folders=config.get("folders",{})
+    path=config.get("settings",{}).get("target_path","").strip('"').strip()
+    path=os.path.normpath(path)
 
-if not if_path_exists(path, "default target directory"):
-    new_path=input("Please enter a valid path to organize files: ")
-    new_path=new_path.strip('"')
-    path=new_path
-    if not if_path_exists(path, f"{path}"):
-        print("Error ❌⚠️: Your provided path is still invalid. Exiting the program.")
-        exit()
-files=os.listdir(path)
-count=0
-for file in files:
-    filename,extension=os.path.splitext(file)
-    extension=extension.lower()
-    for folder in folders:
-        if extension in folders[folder]:
-            folder_paths=os.path.join(path,folder)
-            create_folder(folder,folder_paths)
-            original_file_path=os.path.join(path,file)
-            new_file_path=os.path.join(folder_paths,file) 
-            if os.path.exists(new_file_path): 
-                logging.warning(f"Skipped: {file} (file already exists in {folder} folder)")
-            else:
-                try:
-                    shutil.move(original_file_path,new_file_path)
-                    print(f"Moved: {file} to {folder} folder")
-                    count+=1
-                except Exception as e:
-                    logging.error(f"Error in {file} moving  as :{e}")
-            break 
-if count == 0:
-    summary = "Checking done! \nYour folder is already organized nothing to change! \nFor more details may refer to the LOG file"
+    if not if_path_exists(path, "default target directory"):
+        new_path=input("Please enter a valid path to organize files: ")
+        new_path=new_path.strip('"')
+        path=new_path
+        if not if_path_exists(path, f"{path}"):
+            print("Error ❌⚠️: Your provided path is still invalid. Exiting the program.")
+            exit()
     
-else:
-    summary = f"Checking done✅! Total number of files moved = {count}"
+    files=[f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
+    count=0
+    dry_count=0
+    for file in files:
 
-logging.info(summary)
-print(f"✅{summary}")
-logging.info("***********Scanning Completed***********")
+        filename,extension=os.path.splitext(file)
+        extension=extension.lower()
+        for folder in folders:
+            if extension in folders[folder]:
+                folder_paths=os.path.join(path,folder)
+                create_folder(folder,folder_paths)
+                original_file_path=os.path.join(path,file)
+                new_file_path=os.path.join(folder_paths,file) 
+                if os.path.exists(new_file_path): 
+                    logging.warning(f"Skipped: {file} (file already exists in {folder} folder)")
+                else: 
+                    if is_dry_run:
+                        print(f"Dry run: Moving: {file} to {folder} folder")
+                        logging.info(f"Dry Run: Moved: {file} to {folder} folder")
+                        dry_count+=1
+                    else:
+                        try:
+                            shutil.move(original_file_path,new_file_path)
+                            print(f"Moved: {filename} to {folder} folder")
+                            count+=1
+                        except Exception as e:
+                            logging.error(f"Error in {file} moving  as :{e}")
+                break 
+    if is_dry_run:
+        summary= f"Dry run completed! Total number of files that would be moved = {dry_count}"
+    elif count == 0:
+        summary = "Checking done! \nYour folder is already organized nothing to change! \nFor more details may refer to the LOG file"
+    else:
+        summary = f"Checking done✅! Total number of files moved = {count}"
+
+    logging.info(summary)
+    print(f"✅{summary}")
+    logging.info("***********Scanning Completed***********")
+
+if __name__ == "__main__":
+    main()
